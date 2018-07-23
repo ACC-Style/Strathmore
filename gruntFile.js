2bb91d2378629c134a65cbd654093fde87a9ad6d
Matt Watier <mattwatier@gmail.com>
2018-07-23 12:52:24 -0400

    Adding Foundation Zurb to Project
    Added the base files to strathmore.

--- gruntFile.js
+++ gruntFile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'assets/scss',
-                    src: ['**/*.scss'],
+                    src: ['*.scss'],
                     dest: 'assets/css/',
                     ext: '.css'
                 }]
