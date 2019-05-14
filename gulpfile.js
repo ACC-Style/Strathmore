3fc63b35c313f666a15673ccdde021244479746f
Matt Watier <mattwatier@gmail.com>
2019-05-14 14:14:41 -0400

    Toggle Zurb to UC only
    Removing or placing defaults on the variables used in the UC design.

--- gulpfile.js
+++ gulpfile.js
@@ -18,13 +18,14 @@ var minimist = require('minimist');
 var switchStream = require('stream-switch');
 
 var knownOptions = {
-	string: 'brand',
+	string: 'mode',
 	default: {
-		'brand': "default"
+		'mode': "default"
 	}
 };
 var options = minimist(process.argv.slice(2), knownOptions);
-var buildInst = options.brand;
+var buildInst = options.mode;
+var myConsole = console;
 // Paths
 var PATHS = {
 	SRC: "./",
@@ -44,7 +45,7 @@ var PATHS = {
 
 // Server
 gulp.task("serve", ["stylemin"], function () {
-	console.log("Gulp: Magic there is a browser!");
+	myConsole.log("Gulp: Magic there is a browser!");
 	browserSync.init({
 		server: "./",
 		open: "local",
@@ -57,9 +58,9 @@ gulp.task("serve", ["stylemin"], function () {
 
 // Style Tasks minified and cleaned
 gulp.task("style", function () {
-	console.log("Gulp Style Tasks");
-	console.log("Gulp: I am making this pretty.");
-	console.log(PATHS.SRC + PATHS.SCSS);
+	myConsole.log("Gulp Style Tasks");
+	myConsole.log("Gulp: I am making this pretty.");
+	myConsole.log(PATHS.SRC + PATHS.SCSS);
 	var plugins = [pixrem(), autoprefixer({
 		browsers: ["last 2 version"]
 	})];
@@ -72,9 +73,9 @@ gulp.task("style", function () {
 });
 // Style Tasks minified and cleaned
 gulp.task("stylemin", function () {
-	console.log("Gulp Style Min Tasks");
-	console.log("Gulp: I am folding this into a swan.");
-	console.log(PATHS.SRC + PATHS.SCSS);
+	myConsole.log("Gulp Style Min Tasks");
+	myConsole.log("Gulp: I am folding this into a swan.");
+	myConsole.log(PATHS.SRC + PATHS.SCSS);
 	var plugins = [
 		pixrem(),
 		autoprefixer({
@@ -96,8 +97,8 @@ gulp.task("stylemin", function () {
 
 // Cleaning DIST FOLDER
 gulp.task("clean:dist", ["stylemin"], function () {
-	console.log(' Gulp Cleaning "dist" folder');
-	console.log(
+	myConsole.log(' Gulp Cleaning "dist" folder');
+	myConsole.log(
 		"Gulp: Hold on I am busy cleaning up this mess.  Why do you leave things just lying around."
 	);
 	return gulp
@@ -109,8 +110,8 @@ gulp.task("clean:dist", ["stylemin"], function () {
 
 // Cleaning DOCS FOLDER
 gulp.task("clean:docs", ["styleguide"], function () {
-	console.log(' Gulp Cleaning "docs"');
-	console.log(
+	myConsole.log(' Gulp Cleaning "docs"');
+	myConsole.log(
 		"Gulp: Hold on I am busy cleaning up this mess. Dirty socks on the chandelier? .. really?"
 	);
 	return gulp
@@ -122,22 +123,22 @@ gulp.task("clean:docs", ["styleguide"], function () {
 
 // Script Tasks
 gulp.task("script", function () {
-	console.log(" Gulp Script Tasks");
-	console.log(
+	myConsole.log(" Gulp Script Tasks");
+	myConsole.log(
 		'Gulp: This bit goes here and this attaches here. Now where is tab "Omega"?'
 	);
 });
 
 // Image OPTIMIZE
 gulp.task("images", function () {
-	console.log("Gulp Images Tasks");
-	console.log("Gulp: This is pretty what happens when I smash it!");
+	myConsole.log("Gulp Images Tasks");
+	myConsole.log("Gulp: This is pretty what happens when I smash it!");
 });
 
 // Style Guide Tasks. Build and Copy files for the style guide in local
 gulp.task("styleguide:generate", ["style"], function () {
-	console.log("Gulp Style Guide Tasks");
-	console.log("Gulp: Build the Documentation!");
+	myConsole.log("Gulp Style Guide Tasks");
+	myConsole.log("Gulp: Build the Documentation!");
 	return gulp
 		.src("./assets/css/" + PATHS.SCCS_BUILD_NAME + ".css")
 		.pipe(
@@ -165,8 +166,8 @@ gulp.task("styleguide:generate", ["style"], function () {
 });
 
 gulp.task("styleguide:applystyles", ["package:strathmore"], function () {
-	console.log("Gulp Style Guide Tasks");
-	console.log("Gulp: Write it down and record it!");
+	myConsole.log("Gulp Style Guide Tasks");
+	myConsole.log("Gulp: Write it down and record it!");
 	return gulp
 		.src([
 			"./assets/scss/" + PATHS.SCCS_BUILD_NAME + ".scss",
@@ -183,8 +184,8 @@ gulp.task("styleguide:applystyles", ["package:strathmore"], function () {
 });
 
 gulp.task("package:strathmore", function () {
-	console.log("Gulp Package Tasks");
-	console.log(
+	myConsole.log("Gulp Package Tasks");
+	myConsole.log(
 		"Gulp: Gosh my back is tired. Moving boxes from Assets to the styleguide"
 	);
 	return gulp
@@ -203,11 +204,11 @@ gulp.task("package:strathmore", function () {
 
 // Package Tasks.   Copy files to the Dist Folder.
 gulp.task("package:dist", ["addAutomation"], function () {
-	console.log("Gulp Package Tasks");
-	console.log(
+	myConsole.log("Gulp Package Tasks");
+	myConsole.log(
 		"Gulp: I got a box and some bubble-wrap; now, where is the tape?"
 	);
-	console.log("Gulp: All packed up");
+	myConsole.log("Gulp: All packed up");
 	return gulp
 		.src(
 			[
@@ -226,9 +227,9 @@ gulp.task("package:dist", ["addAutomation"], function () {
 });
 // Package Tasks.   Copy files to the DOCS Folder.
 gulp.task("package:docs", ["clean:docs"], function () {
-	console.log("Gulp Package DOCS");
-	console.log("Gulp: I got a box and tape, but now where is the bubble wrap?");
-	console.log("Gulp: All packed up");
+	myConsole.log("Gulp Package DOCS");
+	myConsole.log("Gulp: I got a box and tape, but now where is the bubble wrap?");
+	myConsole.log("Gulp: All packed up");
 	return gulp
 		.src([PATHS.STYLEGUIDE + "/**/*.*"], {
 			base: "./Strathmore"
@@ -247,31 +248,54 @@ gulp.task("addAutomation", ["clean:dist"], function () {
 		.pipe(rename("gulpfile.js"))
 		.pipe(gulp.dest(PATHS.DIST));
 });
-
+var supportedBrands = function () {
+	return (buildInst == 'acc_zurb' || buildInst == 'acc_boot' || buildInst == 'default' || buildInst == 'cv_boot');
+}
 gulp.task("buildSCSS", function () {
-	console.log("Build is set to default add '--brand' + 'acc_zurb','acc_boot',or cv_boot ");
-
-	gulp.src('assets/scss/index.scss')
-
-		.pipe(switchStream(buildInst, {
-			'acc_zurb': header(fs.readFileSync('assets/scss/gulp_header/_acc-zurb-setup.scss', 'utf8'), {
+	if (buildInst == 'default') {
+		myConsole.warn("You don't have a --mode set.  Build is set to default. Options for --build are acc_zurb,acc_boot,or cv_boot");
+	} else if (supportedBrands()) {
+		myConsole.log("You have --mode set. Building styleguide with selected brand and framework.");
+	} else {
+		myConsole.error("Your --mode is not supported.  Options for --build are acc_zurb,acc_boot,or cv_boot");
+		return;
+	}
+	if (supportedBrands()) {
+		gulp.src('assets/scss/gulp_header/_utility_only.scss')
+			.pipe(switchStream(buildInst, {
+				'acc_zurb': header(fs.readFileSync('assets/scss/components/_zurb.components.scss', 'utf8'), {
+					pkg: pkg
+				}),
+				'default': header(fs.readFileSync('assets/scss/components/_uc-only.components.scss', 'utf8'), {
+					pkg: pkg
+				})
+			}))
+			.pipe(header(fs.readFileSync('assets/scss/components/_components.scss', 'utf8'), {
 				pkg: pkg
-			}),
-			'default': header(fs.readFileSync('assets/scss/gulp_header/_default-setup.scss', 'utf8'), {
+			}))
+			.pipe(header(fs.readFileSync('assets/scss/gulp_header/_base-and-brand.scss', 'utf8'), {
 				pkg: pkg
-			})
-		}))
-		.pipe(header(fs.readFileSync('assets/scss/gulp_header/_preheader.scss', 'utf8'), {
-			pkg: pkg
-		}))
-		.pipe(rename(PATHS.SCCS_BUILD_NAME + ".scss"))
-		.pipe(gulp.dest("assets/scss"));
+			}))
+			.pipe(switchStream(buildInst, {
+				'acc_zurb': header(fs.readFileSync('assets/scss/gulp_header/_acc-zurb-setup.scss', 'utf8'), {
+					pkg: pkg
+				}),
+				'default': header(fs.readFileSync('assets/scss/gulp_header/_default-setup.scss', 'utf8'), {
+					pkg: pkg
+				})
+			}))
+			.pipe(header(fs.readFileSync('assets/scss/gulp_header/_preheader.scss', 'utf8'), {
+				pkg: pkg
+			}))
+			.pipe(rename(PATHS.SCCS_BUILD_NAME + ".scss"))
+			.pipe(gulp.dest("assets/scss"));
+	}
 });
 // Watch Tasks
 gulp.task("watch", ["styleguide"], function () {
-	console.log("Gulp Watch Tasks");
-	console.log("Gulp: I will be watching you.... even when you sleep");
-	gulp.watch([PATHS.SRC + PATHS.SCSS], ["styleguide"]);
+	myConsole.log("Gulp Watch Tasks");
+	myConsole.log("Gulp: I will be watching you.... even when you sleep");
+	gulp.watch([PATHS.SRC + PATHS.SCSS], ["build"]);
 });
 // Default
 gulp.task("default", ["styleguide", "serve", "watch"]);
